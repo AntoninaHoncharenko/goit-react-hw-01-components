35f7755e5418d2c4f46b0b7dbb7d9c86824b8cf1
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-11-26 17:35:43 +0200

    add markup

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <li key={friend.id}>
+          <FriendListItem friend={friend} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
